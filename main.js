65fef4a15f792ea819f08d0f1f40cd83eb08665c
Maryadi <maryadi@astrnt.co>
2016-09-06 06:17:15 +0700

    propTypes

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+
+App.propTypes = {
+    test: React.PropTypes.string,
+    hisoka: React.PropTypes.string,
+    poipo: React.PropTypes.number
+}
+
 ReactDOM.render(
     <div>
         <App test="This is my first property" hisoka="This is the second properties...;)"/>
-        <App test="This is my first 2 property" hisoka="This is the second 2 properties...;)"/>
-        <App test="This is my first 3 property" hisoka="This is the second 3 properties...;)"/>
+
     </div>
     , document.getElementById('app'))

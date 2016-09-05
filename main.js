4a9b055c6fd920ad4120720c2549266f8eea8652
Maryadi <maryadi@astrnt.co>
2016-09-06 06:22:09 +0700

    ngasih nilai ke property number...

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ import App from './App';
 App.propTypes = {
     test: React.PropTypes.string,
     hisoka: React.PropTypes.string,
-    poipo: React.PropTypes.number
+    poipo: React.PropTypes.number.isRequired
 }
 
 ReactDOM.render(
     <div>
-        <App test="This is my first property" hisoka="This is the second properties...;)"/>
+        <App poipo={3} test="This is my first property" hisoka="This is the second properties...;)"/>
 
     </div>
     , document.getElementById('app'))

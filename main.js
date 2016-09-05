7d832c51144e30bed29bc1caa87d683b52012175
Maryadi <maryadi@astrnt.co>
2016-09-06 06:26:07 +0700

    defaultProps,....

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ App.propTypes = {
     poipo: React.PropTypes.number.isRequired
 }
 
+
+App.defaultProps = {
+    test: "This is default value for test",
+    hisoka: "This is default value for hisoka"
+}
+
+
 ReactDOM.render(
     <div>
         <App poipo={3} test="This is my first property" hisoka="This is the second properties...;)"/>

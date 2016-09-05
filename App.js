9fb28b4092b96759abfae97cfac0b656a4ea7551
Maryadi <maryadi@astrnt.co>
2016-09-06 06:08:00 +0700

    App lebih dari 1 di main.js

--- App.js
+++ App.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render()
     {
-        return <div>Hellow Hisoka Poipo ... ;)</div>
+        let myProps = this.props.test
+        return (
+         <div>
+            <h1> Maen React Property </h1>
+            <div>{myProps}</div>
+            <div> Second Props : </div>
+            <div>{this.props.hisoka}</div>
+         </div>
+         )
     }
 }
+
+//const App = () => <h1> Hellow Hisoka Poipo... :D... This is a stateless function in reactjs :D </h1>
 
 export default App
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+    <div>
+        <App test="This is my first property" hisoka="This is the second properties...;)"/>
+        <App test="This is my first 2 property" hisoka="This is the second 2 properties...;)"/>
+        <App test="This is my first 3 property" hisoka="This is the second 3 properties...;)"/>
+    </div>
+    , document.getElementById('app'))
